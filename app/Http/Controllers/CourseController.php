c38b51cab47c232ed65e4396f9ef4d877947bc40
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-03-22 18:50:31 +0700

    create course api

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CourseController extends Controller
 {
     public function GetAllCourse(){
-        $data = Course::all();
+        $data =  DB::table('course')->get();
+        // $data = Course::all();
         if($data){
             return response()->json(['data' =>  $data], 200); 
         }else{
